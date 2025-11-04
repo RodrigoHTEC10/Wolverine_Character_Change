a10f46fa64094927338b10603387bfbf2bed3bdc
Rodrigo Hurtado <A01713854@tec.mx>
2025-11-04 11:21:07 -0600

    File upload of files and images

---
+++ script.js
@@ -0,0 +1,28 @@
+function yellow_button(){
+    document.getElementById("personaje").src = "img/w_1.png";
+    document.getElementById("personaje").style.width = '50%';
+    document.getElementById("personaje").style.bottom = '10px';
+    document.getElementById("fondo").src = "img/fondo_1.png";
+}
+
+function blue_button(){
+    document.getElementById("personaje").src = "img/w_2.png";
+    document.getElementById("personaje").style.width = '40%';
+    document.getElementById("personaje").style.bottom = '10px';
+    document.getElementById("fondo").src = "img/fondo_2.png";
+}
+
+function red_button(){
+    document.getElementById("personaje").src = "img/w_3.png";
+    document.getElementById("personaje").style.width = '40%';
+    document.getElementById("personaje").style.bottom = '100px';
+    document.getElementById("fondo").src = "img/fondo_3.png";
+}
+
+function green_button(){
+    document.getElementById("personaje").src = "img/w_4.png";
+    document.getElementById("personaje").style.width = '40%';
+    document.getElementById("personaje").style.bottom = '10px';
+    document.getElementById("fondo").src = "img/fondo_4.png";
+}
+
